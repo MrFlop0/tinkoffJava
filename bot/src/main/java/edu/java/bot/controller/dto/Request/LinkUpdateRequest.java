48408e00e3c769745a84194b6f473d599a05aef2
MrFlop0 <kaplenkovsasha03@gmail.com>
2024-03-01 21:28:07 +0300

    added Exception handlers for controllers

--- bot/src/main/java/edu/java/bot/controller/dto/Request/LinkUpdateRequest.java
+++ bot/src/main/java/edu/java/bot/controller/dto/Request/LinkUpdateRequest.java
@@ -1,12 +1,17 @@
 package edu.java.bot.controller.dto.Request;
 
+import jakarta.validation.constraints.NotNull;
 import java.net.URI;
 import java.util.List;
 
 public record LinkUpdateRequest(
     Long id,
+
     URI url,
+
     String description,
+
+    @NotNull
     List<Long> tgChatIds
 ) {
 }
